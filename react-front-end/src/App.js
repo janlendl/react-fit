eaa577a2c95fb7e596c90064035a480783894a92
RiazK <RiazCKhan@gmail.com>
2022-02-06 14:58:40 -0500

    Add apiGetRequest FN to GET api data

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -7,6 +7,23 @@ import Workouts from './components/Workouts';
 
 
 function App() {
+
+  let apiGetRequest = {
+    method: 'GET',
+    url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+    headers: {
+      'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
+      'x-rapidapi-key': 'c246264552msh38d3d1e59cd14a4p155348jsnd0dcd0a98213'
+    }
+  };
+
+  axios.request(apiGetRequest)
+    .then((response) => {
+      console.log('I am axios GET res', response.data)
+    }).catch((error) => {
+      console.log(error.message)
+    })
+    
   return (
     <BrowserRouter>
       <Routes>

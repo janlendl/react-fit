e0dd75ab33611b21a2bfc48bf1a4e47fef8f68a9
Lendl Peralta <janlendl@gmail.com>
2022-02-06 23:20:43 -0700

    Create categories query for api call

--- express-back-end/routes/categories.js
+++ express-back-end/routes/categories.js
@@ -1,1 +1,14 @@
-const 
+// This will query all from categories
+
+const router = require("express").Router();
+
+module.exports = db => {
+  router.get("/categories", (req, res) => {
+    db.query(`SELECT * FROM categories`)
+      .then(({ rows: categories }) => {
+      res.json(categories);
+    });
+  });
+
+  return router;
+};

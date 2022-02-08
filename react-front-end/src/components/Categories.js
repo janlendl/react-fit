2558743d61b05ca812a6b270adf9486d47b79c91
gabmor38 <Gabriela.morenor@gmail.co>
2022-02-07 21:23:41 -0500

    fixed columns in Exercise page

--- react-front-end/src/components/Categories.js
+++ react-front-end/src/components/Categories.js
@@ -5,32 +5,40 @@ export default function categories({ }) {
   
     <div className="containerMain categoriesWrapper">
       <div className="workoutCategories">
-        <img src="https://fitonapp.com/wp-content/themes/fiton-20201105/images/Rectangle-7.png"/>
-        <div class="title">Back</div>
+        <div><img src="https://fitonapp.com/wp-content/themes/fiton-20201105/images/Rectangle-7.png"/></div>
+        <div className = "centered">Back</div>
       </div>
       <div className="workoutCategories">
-        Cardio
+        <div><img src="https://fitonapp.com/wp-content/themes/fiton-20201105/images/Rectangle-7.png"/></div>
+        <div className = "centered">Cardio</div>
       </div>
       <div className="workoutCategories">
-        Chest
+        <div><img src="https://fitonapp.com/wp-content/themes/fiton-20201105/images/Rectangle-7.png"/></div>
+        <div className = "centered">Chest</div>
       </div>
       <div className="workoutCategories">
-        Lower Arms
+        <div><img src="https://fitonapp.com/wp-content/themes/fiton-20201105/images/Rectangle-7.png"/></div>
+        <div className = "centered">Lower Arms</div>
       </div>
       <div className="workoutCategories">
-        Lower Legs
+        <div><img src="https://fitonapp.com/wp-content/themes/fiton-20201105/images/Rectangle-7.png"/></div>
+        <div className = "centered">Lower Legs</div>
       </div>
       <div className="workoutCategories">
-        Shoulders
+        <div><img src="https://fitonapp.com/wp-content/themes/fiton-20201105/images/Rectangle-7.png"/></div>
+        <div className = "centered">Shoulders</div>
       </div>
       <div className="workoutCategories">
-        Upper Arms
+        <div><img src="https://fitonapp.com/wp-content/themes/fiton-20201105/images/Rectangle-7.png"/></div>
+        <div className = "centered">Upper Arms</div>
       </div>
       <div className="workoutCategories">
-        Upper Legs
+        <div><img src="https://fitonapp.com/wp-content/themes/fiton-20201105/images/Rectangle-7.png"/></div>
+        <div className = "centered">Upper Legs</div>
       </div>
       <div className="workoutCategories">
-        Core
+        <div><img src="https://fitonapp.com/wp-content/themes/fiton-20201105/images/Rectangle-7.png"/></div>
+        <div className = "centered">Core</div>
       </div>
 
     </div>
--- react-front-end/src/components/Workouts.js
+++ react-front-end/src/components/Workouts.js
@@ -1,23 +1,120 @@
-import "./Workouts.scss"
+import "./Workouts.scss";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import { permittedCrossDomainPolicies } from "helmet";
 
 
-export default function workouts({ props}) {
+
+
+export default function workouts({ props }) {
   return (
-    <div class="WorkoutWrapper">
+    <>
       <div class="row">
-        <div class="col-md-5">
-          <img src="https://fitonapp.com/wp-content/themes/fiton-20201105/images/Rectangle-7.png" class="img-fluid rounded-start" alt="..."/>
+
+        <div class="col-2 text-black">
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Active</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Cardio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Chest</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Lower Arms</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Lower Legs</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Shoulders</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Upper Arms</a>
+            </li>
+          </ul>
         </div>
-        <div class="col-md-6">
-          <div class="card-body">
-            <h5 class="card-title">Exercise Information</h5>
-            <p class="card-text">{props}</p>
- 
+
+        <div class="workoutContainer col-5 clearfix">
+          <div class="card">
+            <div class="row row-cols-2 flex-nowrap">
+              <div class="col-md-5">
+                <img src="https://fitonapp.com/wp-content/themes/fiton-20201105/images/Rectangle-7.png" class="img-fluid rounded-start" alt="..." />
+              </div>
+              <div class="col-md-10">
+                <div class="card-body">
+                  <h5 class="card-title">Exercise Information</h5>
+                  <p class="card-text">{props}</p>
+                  <a href="#" class="btn btn-primary">Add to Workout</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          {/* <div class="card">
+       <div class="card-header">
+        Selected Exercises
+        </div>
+        <div class="row">
+          <img src="https://fitonapp.com/wp-content/themes/fiton-20201105/images/Rectangle-7.png" class="img-fluid rounded-start" alt="..."/>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">An item</li>
+            <li>Sets</li>
+            <li>Reps</li>
+          </ul>
+        </div>
+          <li class="list-group-item">A second item</li>
+          <li class="list-group-item">A third item</li>
+        
+      </div> */}
+        </div>
+        <div class="col-5">
+          <div class='flex bg-light flex flex-column overflow-scroll'>
+            <div class="flex-1 py-6 overflow-auto px-4 sm:px-6">
+              <div class="flex align-items-start justify-content-between">
+                <h2 class="fw-bold text-black">Selected Exercises</h2>
+                <div class="ms-3 h-7 flex align-items-center">
+                  <button type="button" class="btn-close">X</button>
+                </div>
+              </div>
+              <div class="mt-4">
+                <div class="flow-root">
+                  <ul class="list-group mt-4">
+                    <li class="py-6 flex list-group-item">
+                      <div class="flex-shrink-0 ps-1">
+                        <img src="https://fitonapp.com/wp-content/themes/fiton-20201105/images/Rectangle-7.png" class="rounded-0"></img>
+                      </div>
+
+                      <div class="ms-4 flex-1 flex flex-column">
+                        <div>
+                          <div class="flex justify-content-between text-close">
+                            <h3>
+                              <a>Name of product</a>
+                            </h3>
+                          </div>
+                        </div>
+
+                        <div class="flex-1 flex align-items-end justify-content-between fs-5">
+                          <p> QTY</p>
+                          <div class="flex">
+                            <button type="button" class="btn btn-primary">Remove</button>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                    <li>Hello</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    </>
+
+
+
+  );
 }

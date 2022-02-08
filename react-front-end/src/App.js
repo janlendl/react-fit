a7fa0cf9f9f0cafeaa8f67e2a5d70fdb9a1fe878
RiazK <RiazCKhan@gmail.com>
2022-02-08 09:03:20 -0500

    Create 'Exercises' route

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,26 +1,22 @@
-import axios from 'axios';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route} from "react-router-dom"
 import Categories from './components/Categories'
 import Navbar from './components/Navbar';
 import Workouts from './components/Workouts';
+import Exercises from './components/Exercises';
 
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navbar/>}>
         <Route index element={<Categories/>}/>
         <Route path="Workouts" element={<Workouts/>}/>
+        <Route path="Exercises" element={<Exercises/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
-    // <div>
-    // <Navbar/>
-    // <Categories/>
-    // </div>
   );
 }
 export default App;

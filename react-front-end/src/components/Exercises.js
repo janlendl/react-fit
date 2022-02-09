9d306f2618576281ff37ffde1110c5541d6b3d72
RiazK <RiazCKhan@gmail.com>
2022-02-09 17:08:59 -0500

    Merge Workouts component styling into Exercises component

--- react-front-end/src/components/Exercises.js
+++ react-front-end/src/components/Exercises.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import { Link, useParams } from "react-router-dom"
 import { useState, useEffect } from "react";
 
+import "./Workouts.scss";
+
 export default function Exercises(props) {
   console.log("You've reached the Exercises Page")
 
@@ -31,65 +33,122 @@ export default function Exercises(props) {
 
   return (
 
-    <section className="containerMain categoriesWrapper">
-
-      <article>
-        <Link to="/exercises/back">
-          <button>back</button>
-        </Link>
-      </article>
-
-      <article>
-        <Link to="/exercises/cardio">
-          <button>cardio</button>
-        </Link>
-      </article>
-
-      <article>
-        <Link to="/exercises/chest">
-          <button>chest</button>
-        </Link>
-      </article>
-
-      <article>
-        <Link to="/exercises/lower%20arms">
-          <button>lower arms</button>
-        </Link>
-      </article>
-
-      <article>
-        <Link to="/exercises/lower%20legs">
-          <button>lower legs</button>
-        </Link>
-      </article>
-
-      <article>
-        <Link to="/exercises/shoulders">
-          <button>shoulders</button>
-        </Link>
-      </article>
-
-      <article>
-        <Link to="/exercises/upper%20arms">
-          <button>upper arms</button>
-        </Link>
-      </article>
-
-      <article>
-        <Link to="/exercises/upper%20legs">
-          <button>upper legs</button>
-        </Link>
-      </article>
-
-      <article>
-        <Link to="/exercises/waist">
-          <button>core</button>
-        </Link>
-      </article>
-
-    </section>
+    <>
+      <div className="topWrapper">
+        <div className="row">
+          <div className="col-1 text-black sidebar fle flex-column boxstyle">
+            <h3>Categories</h3>
+            <ul className="nav flex-column">
+              <li className="nav-item">
+                <Link to="/exercises/back">
+                  <span className="nav-link">Back</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/exercises/cardio">
+                  <span className="nav-link">Cardio</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/exercises/chest">
+                  <span className="nav-link">Chest</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/exercises/lower%20arms">
+                  <span className="nav-link">Lower Arms</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/exercises/lower%20legs">
+                  <span className="nav-link">Lower Legs</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/exercises/shoulders">
+                  <span className="nav-link">Shoulders</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/exercises/upper%20arms">
+                  <span className="nav-link">Upper Arms</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/exercises/upper%20legs">
+                  <span className="nav-link">Upper Legs</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/exercises/waist">
+                  <span className="nav-link">Core</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
 
 
+          <div className="workoutContainer col-4 clearfix">
+            <div className="card boxstyle">
+              <div className="row row-cols-2 flex-nowrap">
+                <div className="col-md-5">
+                  <img src="https://fitonapp.com/wp-content/themes/fiton-20201105/images/Rectangle-7.png" className="img-fluid rounded-start" alt="..." />
+                </div>
+                <div className="col-md-10">
+                  <div className="card-body">
+                    <h5 className="card-title">Exercise Information</h5>
+                    <p className="card-text">TO BE REPLACED BY PROPS</p>
+                    <a href="#" className="btn btn-primary">Add to Workout</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <div className="col-5">
+            <div className='flex bg-light flex flex-column overflow-scroll boxstyle'>
+              <div className="flex-1 py-6 overflow-auto px-4 sm:px-6">
+                <div className="flex align-items-start justify-content-between">
+                  <h2 className="fw-bold text-black">Selected Exercises</h2>
+                  <div className="ms-3 h-7 flex align-items-center">
+                    <button type="button" className="btn-close">X</button>
+                  </div>
+                </div>
+                <div className="mt-4">
+                  <div className="flow-root">
+                    <ul className="list-group mt-4">
+                      <li className="py-6 flex list-group-item purple">
+                        <div className="flex-shrink-0 ps-1">
+                          <img src="https://fitonapp.com/wp-content/themes/fiton-20201105/images/Rectangle-7.png" className="rounded-0 mrg-right sizeImg"></img>
+                        </div>
+
+                        <div className="ms-4 flex-1 flex flex-column">
+                          <div>
+                            <div className="flex justify-content-between text-close">
+                              <h3>
+                                <a>Name of product</a>
+                              </h3>
+                            </div>
+                          </div>
+
+                          <div className="flex-1 flex align-items-end justify-content-between fs-5">
+                            <p> QTY</p>
+                            <div className="flex">
+                              <button type="button" className="btn btn-primary">Remove</button>
+                            </div>
+                          </div>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </>
   )
 }
 

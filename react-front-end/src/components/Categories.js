0b10f2a4dc8b300aa0c9b00e4e3249fdb0735a03
Gareth <gar.carey@gmail.com>
2022-02-08 19:17:07 -0500

    small refactor of layout

--- react-front-end/src/components/Categories.js
+++ react-front-end/src/components/Categories.js
@@ -1,42 +1,20 @@
-import "./CategoriesStyle.scss"
+import "./CategoriesStyle.scss";
 
-export default function categories({ }) {
+export default function categories({}) {
   return (
-  
     <div className="containerMain categoriesWrapper">
       <div className="workoutCategories">
-        <img src="https://fitonapp.com/wp-content/themes/fiton-20201105/images/Rectangle-7.png"/>
+        <img src="https://fitonapp.com/wp-content/themes/fiton-20201105/images/Rectangle-7.png" />
         <div class="title">Back</div>
       </div>
-      <div className="workoutCategories">
-        Cardio
-      </div>
-      <div className="workoutCategories">
-        Chest
-      </div>
-      <div className="workoutCategories">
-        Lower Arms
-      </div>
-      <div className="workoutCategories">
-        Lower Legs
-      </div>
-      <div className="workoutCategories">
-        Shoulders
-      </div>
-      <div className="workoutCategories">
-        Upper Arms
-      </div>
-      <div className="workoutCategories">
-        Upper Legs
-      </div>
-      <div className="workoutCategories">
-        Core
-      </div>
-
+      <div className="workoutCategories">Cardio</div>
+      <div className="workoutCategories">Chest</div>
+      <div className="workoutCategories">Lower Arms</div>
+      <div className="workoutCategories">Lower Legs</div>
+      <div className="workoutCategories">Shoulders</div>
+      <div className="workoutCategories">Upper Arms</div>
+      <div className="workoutCategories">Upper Legs</div>
+      <div className="workoutCategories">Core</div>
     </div>
-
-
-
-    
-  )
+  );
 }

5b1e0e1e51a39662b726fc6c549e3c82fd70e563
Gareth <gar.carey@gmail.com>
2022-02-08 19:20:52 -0500

    remove class import and classes for div and h1 as causing errors with no about.css currently

--- react-front-end/src/components/about.js
+++ react-front-end/src/components/about.js
@@ -1,16 +1,13 @@
-import classes from '../pages/AboutUs.css'
-
-
 export default function About() {
   return (
-<>
-    <div className={classes.container}>
-     <h1 className= {classes.title}>About Us</h1>
-    </div>
+    <>
+      <div>
+        <h1>About Us</h1>
+      </div>
 
-    <div>
-      <img></img>
-    </div>
-</>  
-  )
+      <div>
+        <img></img>
+      </div>
+    </>
+  );
 }

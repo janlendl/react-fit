7c24e4e99a4d79c346ecee2bf7f3b7068340b4eb
Gareth <gar.carey@gmail.com>
2022-02-08 19:14:49 -0500

    add About page route to nav bar

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,18 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route} from "react-router-dom"
-import Categories from './components/Categories'
-import Navbar from './components/Navbar';
-import Workouts from './components/Workouts';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Categories from "./components/Categories";
+import Navbar from "./components/Navbar";
+import Workouts from "./components/Workouts";
+import About from "./components/about";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navbar/>}>
-        <Route index element={<Categories/>}/>
-        <Route path="Workouts" element={<Workouts/>}/>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Categories />} />
+          <Route path="Workouts" element={<Workouts />} />
+          <Route path="About" element={<About />} />
         </Route>
       </Routes>
     </BrowserRouter>

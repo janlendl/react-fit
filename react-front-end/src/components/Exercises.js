3fc091387899af78af046aa728e100726d81b9d6
RiazK <RiazCKhan@gmail.com>
2022-02-10 15:18:39 -0500

    Removed Api call due to termination of incoming requests, i.e., no more requests. Replaced with identical dummy data

--- react-front-end/src/components/Exercises.js
+++ react-front-end/src/components/Exercises.js
@@ -3,12 +3,15 @@ import { Link, useParams } from "react-router-dom"
 import { useState, useEffect } from "react";
 
 import "./Exercises.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export default function Exercises(props) {
   console.log("You've reached the Exercises Page")
 
   let { category } = useParams();
 
+  // Variable exerciseData will be plugged into the embbed map to represent API Data -- See note below
   const [exerciseData, setExerciseData] = useState([])
 
   let apiExerciseByBodyPart = {
@@ -28,6 +31,40 @@ export default function Exercises(props) {
     getExercises();
   }, [category])
 
+  const backExercises = [
+    {
+      bodyPart: "back",
+      equipment: "cable",
+      gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0007.gif",
+      id: "0007",
+      name: "alternate lateral pulldown",
+      target: "lats"
+    },
+    {
+      bodyPart: "back",
+      equipment: "body weight",
+      gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3293.gif",
+      id: "3293",
+      name: "archer pull up",
+      target: "lats"
+    },
+    {
+      bodyPart: "back",
+      equipment: "leverage machine",
+      gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0015.gif",
+      id: "0015",
+      name: "assisted parallel close grip pull-up",
+      target: "lats"
+    },
+    {
+      bodyPart: "back",
+      equipment: "leverage machine",
+      gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0017.gif",
+      id: "0017",
+      name: "assisted pull-up",
+      target: "lats"
+    }
+  ]
 
   return (
 
@@ -88,43 +125,38 @@ export default function Exercises(props) {
           <div className="workoutContainer col-4 clearfix">
             <div className="card boxstyle">
               <div className="row row-cols-2 flex-nowrap">
-
                 <table>
-
-                {exerciseData.map((exercise) => {
-                  return (
-
-                  <tr className="exerciseContainer">
-                      <div className="col-md-5">
+                  {/* Variable backExercises will need to be replaced with Variable exerciseData. This has been temporarily removed as I have run out of API requests */}
+                  {backExercises.map((exercise) => {
+                    return (
+                      <td className="exerciseContainer">
+                        <div className="col-md-5">
                           <h5 className="card-title">{exercise.name}</h5>
-                        <img src={exercise.gifUrl} className="img-fluid rounded-start" alt={exercise.name} />
-                      </div>
-                      <div className="exerciseInfo col-md-10">
-                        <div className="card-body">
-
-                    <div class="exerciseContainer">
-                          <ul className="card-text">
-                            <li>
-                              Category: {exercise.bodyPart}
-                            </li>
-                            <li>
-                              Target: {exercise.target}
-                            </li>
-                            <li>
-                              Equipment: {exercise.equipment}
-                            </li>
-                          </ul>
-                    </div>
-
-                          <a className="btn btn-primary">Add Exercise</a>
+                          <img src={exercise.gifUrl} className="img-fluid rounded-start" alt={exercise.name} />
                         </div>
-                      </div>
-                  </tr>  
+                        <div className="exerciseInfo col-md-10">
+                          <div className="card-body">
 
-                  )
-                })}
-                    </table>
-
+                            <div className="exerciseContainer">
+                              <ul className="card-text">
+                                <li>
+                                  Category: {exercise.bodyPart}
+                                </li>
+                                <li>
+                                  Target: {exercise.target}
+                                </li>
+                                <li>
+                                  Equipment: {exercise.equipment}
+                                </li>
+                              </ul>
+                            </div>
+                            <a className="btn btn-primary">Add Exercise</a>
+                          </div>
+                        </div>
+                      </td>
+                    )
+                  })}
+                </table>
               </div>
             </div>
           </div>
@@ -135,7 +167,7 @@ export default function Exercises(props) {
                 <div className="flex align-items-start justify-content-between">
                   <h2 className="fw-bold text-black">Selected Exercises</h2>
                   <div className="ms-3 h-7 flex align-items-center">
-                    <button type="button" className="btn-close">X</button>
+                    {/* <button type="button" className="btn-close">X</button> */}
                   </div>
                 </div>
                 <div className="mt-4">
@@ -143,25 +175,31 @@ export default function Exercises(props) {
                     <ul className="list-group mt-4">
                       <li className="py-6 flex list-group-item purple">
                         <div className="flex-shrink-0 ps-1">
-                          <img src="https://fitonapp.com/wp-content/themes/fiton-20201105/images/Rectangle-7.png" className="rounded-0 mrg-right sizeImg"></img>
+                          {/* <img src="https://fitonapp.com/wp-content/themes/fiton-20201105/images/Rectangle-7.png" className="rounded-0 mrg-right sizeImg"></img> */}
                         </div>
 
+{/* Begining of Exercise List */}
                         <div className="ms-4 flex-1 flex flex-column">
                           <div>
                             <div className="flex justify-content-between text-close">
-                              <h3>
-                                <a>Name of product</a>
-                              </h3>
+                              <span>
+                              Exercise Name
+                              </span>
+                              <form>
+                              Sets
+                              Reps
+                              </form>
+                              <FontAwesomeIcon icon={faTrash} />
                             </div>
                           </div>
 
                           <div className="flex-1 flex align-items-end justify-content-between fs-5">
-                            <p> QTY</p>
                             <div className="flex">
-                              <button type="button" className="btn btn-primary">Remove</button>
                             </div>
                           </div>
                         </div>
+{/* End of Exercise List */}
+
                       </li>
                     </ul>
                   </div>
@@ -169,6 +207,8 @@ export default function Exercises(props) {
               </div>
             </div>
           </div>
+
+
         </div>
       </div>
 
@@ -178,40 +218,7 @@ export default function Exercises(props) {
 
 // Mimi API request for List Exercise By Body Part = Back
 
-// const backExercises = [
-//   {
-//     bodyPart: "back",
-//     equipment: "cable",
-//     gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0007.gif",
-//     id: "0007",
-//     name: "alternate lateral pulldown",
-//     target: "lats"
-//   },
-//   {
-//     bodyPart: "back",
-//     equipment: "body weight",
-//     gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3293.gif",
-//     id: "3293",
-//     name: "archer pull up",
-//     target: "lats"
-//   },
-//   {
-//     bodyPart: "back",
-//     equipment: "leverage machine",
-//     gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0015.gif",
-//     id: "0015",
-//     name: "assisted parallel close grip pull-up",
-//     target: "lats"
-//   },
-//   {
-//     bodyPart: "back",
-//     equipment: "leverage machine",
-//     gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0017.gif",
-//     id: "0017",
-//     name: "assisted pull-up",
-//     target: "lats"
-//   }
-// ]
+
 
 // Mimic API request for List All Body Parts
 

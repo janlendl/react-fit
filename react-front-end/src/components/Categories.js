de598b83e15654bc4ca8a7686943d089c0de3d7c
RiazK <RiazCKhan@gmail.com>
2022-02-09 19:59:53 -0500

    Remove duplicate style 'workoutCategories'

--- react-front-end/src/components/Categories.js
+++ react-front-end/src/components/Categories.js
@@ -8,63 +8,63 @@ export default function Categories() {
 
     <section className="containerMain categoriesWrapper">
 
-      <article className="workoutCategories">
+      <article className="landingPageCategories">
         <Link to="/exercises/back">
           <img src="/back.jpeg" />
           <div className="title">Back</div>
         </Link>
       </article>
 
-      <article className="workoutCategories">
+      <article className="landingPageCategories">
         <Link to="/exercises/cardio">
           <img src="/cardio.jpeg" />
           <div className="title">Cardio</div>
         </Link>
       </article>
 
-      <article className="workoutCategories">
+      <article className="landingPageCategories">
         <Link to="/exercises/chest">
           <img src="/chest.jpeg" />
           <div className="title">Chest</div>
         </Link>
       </article>
 
-      <article className="workoutCategories">
+      <article className="landingPageCategories">
         <Link to="/exercises/lower%20arms">
           <img src="/lower_arm.jpeg" />
           <div className="title">Lower Arms</div>
         </Link>
       </article>
 
-      <article className="workoutCategories">
+      <article className="landingPageCategories">
         <Link to="/exercises/lower%20legs">
           <img src="/lower_leg.jpeg" />
           <div className="title">Lower Legs</div>
         </Link>
       </article>
 
-      <article className="workoutCategories">
+      <article className="landingPageCategories">
         <Link to="/exercises/shoulders">
           <img src="/shoulder.jpeg" />
           <div className="title">Shoulders</div>
         </Link>
       </article>
 
-      <article className="workoutCategories">
+      <article className="landingPageCategories">
         <Link to="/exercises/upper%20arms">
           <img src="/upper_arm.jpeg" />
           <div className="title">Upper Arms</div>
         </Link>
       </article>
 
-      <article className="workoutCategories">
+      <article className="landingPageCategories">
         <Link to="/exercises/upper%20legs">
           <img src="/upper_leg.jpeg" />
           <div className="title">Upper Legs</div>
         </Link>
       </article>
 
-      <article className="workoutCategories">
+      <article className="landingPageCategories">
         <Link to="/exercises/waist">
           <img src="/core.jpeg" />
           <div className="title">Core</div>

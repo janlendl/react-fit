d3640dd01cce06a9a006e9d20e86cbffd911793b
Lendl Peralta <janlendl@gmail.com>
2022-02-10 21:49:44 -0700

    Initial creation of WorkoutListItem

---
+++ react-front-end/src/components/WorkoutListItem.js
@@ -0,0 +1,17 @@
+import './WorkoutListItem.scss';
+
+export default function WorkoutListItem() {
+  return (
+    <>
+      <h2>WorkoutName</h2>
+      <li>date created</li>
+      <li>gif</li>
+      <li>exercise name</li>
+      <li>equipment</li>
+      <li>body part</li>
+      <li>target</li>
+      <li>number of sets</li>
+      <li>number of reps</li>
+    </>
+  );
+}

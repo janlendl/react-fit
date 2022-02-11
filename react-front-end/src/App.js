73066d90f598d4554ea5ef31cab8873537f920d9
RiazK <RiazCKhan@gmail.com>
2022-02-10 21:08:57 -0500

    Update Exercise Parent Route

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Categories from "./components/Categories";
-import Exercises from "./components/Exercises";
+import ExerciseList from "./components/ExerciseList";
 import Navbar from "./components/Navbar";
 import Workouts from "./components/Workouts";
 import About from "./components/About";
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<Navbar/>}>
         <Route index element={<Categories/>}/>
         <Route path="/about" element={<About/>}/>
-        <Route path="/exercises/:category" element={<Exercises/>}/>
+        <Route path="/exercises/:category" element={<ExerciseList/>}/>
         <Route path="/workouts" element={<Workouts/>}/>
         </Route>
       </Routes>

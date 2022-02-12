19787e136bd3c0b12c8c7dfa7ea2cbaa703662e1
Lendl Peralta <janlendl@gmail.com>
2022-02-12 05:56:49 +0000

    Update mock data and elements to be rendered in the component

--- react-front-end/src/components/WorkoutList.js
+++ react-front-end/src/components/WorkoutList.js
@@ -1,61 +1,78 @@
 import WorkoutListItem from "./WorkoutListItem";
 
 const workoutData = [
-{
-  exercise_id: 1,
-  workout_name: "Terror Thursday",
-  created_date: "2022-02-03T07:00:00.000Z",
-  exercise_name: "alternate lateral pulldown",
-  gifurl: "http://d205bpvrqc9yn1.cloudfront.net/0007.gif",
-  equipment: "cable",
-  number_of_sets: 3,
-  number_of_reps: 6
-  },
   {
-  exercise_id: 2,
-  workout_name: "Terror Thursday",
-  created_date: "2022-02-01T07:00:00.000Z",
-  exercise_name: "archer pull up",
-  gifurl: "http://d205bpvrqc9yn1.cloudfront.net/3293.gif",
-  equipment: "body weight",
-  number_of_sets: 3,
-  number_of_reps: 10
-  },
-  {
-  exercise_id: 3,
-  workout_name: "Terror Thursday",
-  created_date: "2022-02-03T07:00:00.000Z",
-  exercise_name: "astride jumps (male)",
-  gifurl: "http://d205bpvrqc9yn1.cloudfront.net/3220.gif",
-  equipment: "body weight",
-  number_of_sets: 2,
-  number_of_reps: 12
-  },
-  {
-  exercise_id: 4,
-  workout_name: "Terror Thursday",
-  created_date: "2022-02-01T07:00:00.000Z",
-  exercise_name: "back and forth step",
-  gifurl: "http://d205bpvrqc9yn1.cloudfront.net/3672.gif",
-  equipment: "body weight",
-  number_of_sets: 4,
-  number_of_reps: 12
+    workout_id: 1,
+    workout_name: "Terror Thursday",
+    created_date: "2022-02-03T07:00:00.000Z",
+    exercise: [
+      {
+      name: "alternate lateral pulldown",
+      gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0007.gif",
+      body_part: "back",
+      equipment: "cable",
+      target_muscle: "lats",
+      number_of_sets: 5,
+      number_of_reps: 5
+      },
+      {
+      name: "astride jumps (male)",
+      gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3220.gif",
+      body_part: "cardio",
+      equipment: "body weight",
+      target_muscle: "cardiovascular system",
+      number_of_sets: 10,
+      number_of_reps: 10
+      },
+      {
+      name: "archer push up",
+      gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3294.gif",
+      body_part: "chest",
+      equipment: "body weight",
+      target_muscle: "pectorals",
+      number_of_sets: 12,
+      number_of_reps: 4
+      },
+      {
+      name: "band reverse wrist curl",
+      gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0994.gif",
+      body_part: "lower_arms",
+      equipment: "band",
+      target_muscle: "forearms",
+      number_of_sets: 6,
+      number_of_reps: 3
+      },
+      {
+      name: "ankle circles",
+      gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/1368.gif",
+      body_part: "lower_legs",
+      equipment: "body weight",
+      target_muscle: "calves",
+      number_of_sets: 10,
+      number_of_reps: 5
+      },
+      {
+      name: "neck side stretch",
+      gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/1403.gif",
+      body_part: "neck",
+      equipment: "body weight",
+      target_muscle: "levator scapulae",
+      number_of_sets: 8,
+      number_of_reps: 2
+      }
+    ]
   }
-];
+];  
 
 export default function WorkoutList() {
 
   const workoutList = workoutData.map((workout) => {
     return (
       <WorkoutListItem
-        key={workout.exercise_id}
+        key={workout.workout_id}
         workoutName={workout.workout_name}
         dateCreated={workout.created_date}
-        exerciseName={workout.exercise_name}
-        gif={workout.gifurl}
-        equipment={workout.equipment}
-        numSets={workout.number_of_sets}
-        numReps={workout.number_of_reps}
+        exercise={workout.exercise}
       />
     )
   })

fb633f56144aaf25075f6716892d81905ef1a928
Lendl Peralta <janlendl@gmail.com>
2022-02-12 05:42:52 +0000

    Update query to include a where clause for user id

--- express-back-end/routes/workouts.js
+++ express-back-end/routes/workouts.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 module.exports = db => {
   router.get("/workouts", (req, res) => {
     db.query(`
-    SELECT workouts.id AS workout_id, workout_name, created_date, 
+    SELECT workouts.id AS workout_id, workout_name, created_date,
     CASE WHEN exercises.id IS NULL
     THEN NULL
     ELSE json_build_object('name', exercises.exercise_name, 'gifUrl', exercises.gifUrl, 'body_part', categories.category_name, 'equipment', exercises.equipment, 'target_muscle', exercises.target_muscle, 'number_of_sets', exercises.number_of_sets, 'number_of_reps', exercises.number_of_reps)
@@ -13,6 +13,9 @@ module.exports = db => {
     LEFT JOIN exercise_workouts ON exercise_id = exercises.id
     JOIN workouts ON workout_id = workouts.id
     JOIN categories ON category_id = categories.id
+    JOIN user_workouts ON user_workouts.workout_id = workouts.id
+    JOIN users on user_workouts.user_id = users.id
+    WHERE user_id = 1
     GROUP BY workouts.id, workout_name, exercises.id, category_name
     ORDER BY exercises.id;
     `)

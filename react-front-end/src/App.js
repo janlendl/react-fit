4fde35914e68c9ea0849762fe952142ec8c1ccc9
Gareth <gar.carey@gmail.com>
2022-02-12 09:36:20 -0500

    add user profile route

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -6,17 +6,18 @@ import Exercises from "./components/Exercises";
 import Navbar from "./components/Navbar";
 import Workouts from "./components/Workouts";
 import About from "./components/About";
+import Profile from "./components/Profile";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navbar/>}>
-        <Route index element={<Categories/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/exercises/:category" element={<Exercises/>}/>
-        <Route path="/workouts" element={<Workouts/>}/>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Categories />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/exercises/:category" element={<Exercises />} />
+          <Route path="/workouts" element={<Workouts />} />
+          <Route path="/profile" element={<Profile />} />
         </Route>
       </Routes>
     </BrowserRouter>

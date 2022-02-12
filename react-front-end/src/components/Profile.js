f6e0d0d1aba49a5a663f2b57b1974a6d8a83d0d6
Gareth <gar.carey@gmail.com>
2022-02-12 09:37:09 -0500

    add Profile page

---
+++ react-front-end/src/components/Profile.js
@@ -0,0 +1,11 @@
+import "./Profile.scss";
+
+export default function Profile() {
+  return (
+    <main>
+      <div className="background">
+        <div className="profile"></div>
+      </div>
+    </main>
+  );
+}

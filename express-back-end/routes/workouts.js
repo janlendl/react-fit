6f6d852cd3b738d2fce8d5ec45b7453653072e4a
Lendl Peralta <janlendl@gmail.com>
2022-02-13 05:23:16 +0000

    Add querty to create new workout, and delete workout

--- express-back-end/routes/workouts.js
+++ express-back-end/routes/workouts.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 // this will query all the exercises based on the workout name
 const router = require("express").Router();
 
@@ -37,6 +39,53 @@ module.exports = db => {
       res.json(Object.values(exerciseObj));
     });
   });
+
+
+  router.put("/workouts/:id", (req, res) => {
+    
+    const {newWorkout} = req.body.createWorkout;
+
+    db.query(`
+      WITH new_workout as (
+        INSERT INTO workouts (workout_name, created_date) VALUES ($1::varchar, $2::date)
+        RETURNING id
+      )
+      ,new_exercise (
+        INSERT INTO exercises (exercise_name, gifUrl, target_muscle, equipment, number_of_sets, number_of_reps, category_id) VALUES ($3::varchar, $4::varchar, $5::varchar, $6::varchar, $7::int, $8::int, $9::int)
+        RETURNING id
+      )
+      ,new_user_workout (
+        INSERT INTO user_workouts (user_id, workout_id) VALUES (1, (SELECT id from new_workout))
+      )
+      INSERT INTO exercise_workouts (exercise_id, workout_id) VALUES((SELECT id from new_exercise), (SELECT id from new_workout));
+      `, 
+      [newWorkout]
+    )
+      .then(() => {
+        setTimeout(() => {
+          response.status(204).json({res});
+          console.log("New Workout Created!");
+        }, 1000);
+      })
+      .catch(error => console.log('Error: ', error)); 
+  });
+
+  router.delete("/workouts/:id", (req, res) => {
+    
+    const {workout} = req.body.workout;
+    
+    db.query(`
+      DELETE FROM workouts WHERE workouts.id = $1::int; 
+    `, [req.params.id]
+    )
+      .then(() => {
+        setTimeout(() => {
+          response.status(204).json({res});
+          console.log("Workout Deleted!");
+        }, 1000);      
+      })
+      .catch(error => console.log('Error', error));
+  })
 
   return router;
 };

0842d83329338d726c66e17a4976ff055af001ae
gabmor38 <Gabriela.morenor@gmail.co>
2022-02-14 21:08:26 -0500

    fixed layout

--- react-front-end/src/components/WorkoutList.js
+++ react-front-end/src/components/WorkoutList.js
@@ -1,4 +1,6 @@
 import WorkoutListItem from "./WorkoutListItem";
+import "./Exercises.scss";
+import "./Workouts.scss";
 
 const workoutData = [
   {
@@ -7,62 +9,123 @@ const workoutData = [
     created_date: "2022-02-03T07:00:00.000Z",
     exercise: [
       {
-      name: "alternate lateral pulldown",
-      gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0007.gif",
-      body_part: "back",
-      equipment: "cable",
-      target_muscle: "lats",
-      number_of_sets: 5,
-      number_of_reps: 5
+        name: "alternate lateral pulldown",
+        gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0007.gif",
+        body_part: "back",
+        equipment: "cable",
+        target_muscle: "lats",
+        number_of_sets: 5,
+        number_of_reps: 5
       },
       {
-      name: "astride jumps (male)",
-      gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3220.gif",
-      body_part: "cardio",
-      equipment: "body weight",
-      target_muscle: "cardiovascular system",
-      number_of_sets: 10,
-      number_of_reps: 10
+        name: "astride jumps (male)",
+        gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3220.gif",
+        body_part: "cardio",
+        equipment: "body weight",
+        target_muscle: "cardiovascular system",
+        number_of_sets: 10,
+        number_of_reps: 10
       },
       {
-      name: "archer push up",
-      gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3294.gif",
-      body_part: "chest",
-      equipment: "body weight",
-      target_muscle: "pectorals",
-      number_of_sets: 12,
-      number_of_reps: 4
+        name: "archer push up",
+        gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3294.gif",
+        body_part: "chest",
+        equipment: "body weight",
+        target_muscle: "pectorals",
+        number_of_sets: 12,
+        number_of_reps: 4
       },
       {
-      name: "band reverse wrist curl",
-      gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0994.gif",
-      body_part: "lower_arms",
-      equipment: "band",
-      target_muscle: "forearms",
-      number_of_sets: 6,
-      number_of_reps: 3
+        name: "band reverse wrist curl",
+        gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0994.gif",
+        body_part: "lower_arms",
+        equipment: "band",
+        target_muscle: "forearms",
+        number_of_sets: 6,
+        number_of_reps: 3
       },
       {
-      name: "ankle circles",
-      gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/1368.gif",
-      body_part: "lower_legs",
-      equipment: "body weight",
-      target_muscle: "calves",
-      number_of_sets: 10,
-      number_of_reps: 5
+        name: "ankle circles",
+        gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/1368.gif",
+        body_part: "lower_legs",
+        equipment: "body weight",
+        target_muscle: "calves",
+        number_of_sets: 10,
+        number_of_reps: 5
       },
       {
-      name: "neck side stretch",
-      gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/1403.gif",
-      body_part: "neck",
-      equipment: "body weight",
-      target_muscle: "levator scapulae",
-      number_of_sets: 8,
-      number_of_reps: 2
+        name: "neck side stretch",
+        gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/1403.gif",
+        body_part: "neck",
+        equipment: "body weight",
+        target_muscle: "levator scapulae",
+        number_of_sets: 8,
+        number_of_reps: 2
+      }
+    ]
+  },
+  {
+    workout_id: 1,
+    workout_name: "Sunday Cardio",
+    created_date: "2022-02-03T07:00:00.000Z",
+    exercise: [
+      {
+        name: "alternate lateral pulldown",
+        gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0007.gif",
+        body_part: "back",
+        equipment: "cable",
+        target_muscle: "lats",
+        number_of_sets: 5,
+        number_of_reps: 5
+      },
+      {
+        name: "astride jumps (male)",
+        gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3220.gif",
+        body_part: "cardio",
+        equipment: "body weight",
+        target_muscle: "cardiovascular system",
+        number_of_sets: 10,
+        number_of_reps: 10
+      },
+      {
+        name: "archer push up",
+        gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3294.gif",
+        body_part: "chest",
+        equipment: "body weight",
+        target_muscle: "pectorals",
+        number_of_sets: 12,
+        number_of_reps: 4
+      },
+      {
+        name: "band reverse wrist curl",
+        gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0994.gif",
+        body_part: "lower_arms",
+        equipment: "band",
+        target_muscle: "forearms",
+        number_of_sets: 6,
+        number_of_reps: 3
+      },
+      {
+        name: "ankle circles",
+        gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/1368.gif",
+        body_part: "lower_legs",
+        equipment: "body weight",
+        target_muscle: "calves",
+        number_of_sets: 10,
+        number_of_reps: 5
+      },
+      {
+        name: "neck side stretch",
+        gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/1403.gif",
+        body_part: "neck",
+        equipment: "body weight",
+        target_muscle: "levator scapulae",
+        number_of_sets: 8,
+        number_of_reps: 2
       }
     ]
   }
-];  
+];
 
 export default function WorkoutList() {
 
@@ -74,12 +137,38 @@ export default function WorkoutList() {
         dateCreated={workout.created_date}
         exercises={workout.exercise}
       />
-    )
-  })
+    );
+  });
 
   return (
-    <div className="workoutlist">
-    {workoutList}
-    </div>
-  )
+    <>
+      <div className="topWrapper"></div>
+      <div className="container-md">
+      <div classname="row noMrg">
+        <div className="col-lg-7 d-flex">
+          
+            <div className="card-text d-flex">
+              {workoutList}
+
+           
+          </div>
+        </div>
+      </div>
+      </div>
+    </>
+    // {/* 
+    //   <div className="container mt-4 pt-4">
+    //     <div className="row">
+    //       <div className="d-flex flex-column">
+    //       {workoutList}
+    //       </div>
+
+
+    //     </div>
+    // </div> */}
+
+
+
+
+  );
 }

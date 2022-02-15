6928e1fbb98810650472a71edadbd3f8763951bd
gabmor38 <Gabriela.morenor@gmail.co>
2022-02-14 21:10:19 -0500

    commented all the code not needed

--- react-front-end/src/components/Workouts.js
+++ react-front-end/src/components/Workouts.js
@@ -1,74 +1,73 @@
 
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+// import "./Exercises.scss";
 
 
 
+// export default function Workouts(props) {
+
+//   const [exerciseList, setaddExerciseList] = useState([]);
+//   console.log("this is the added Items", exerciseList);
+//   const [repCount, setRepCount] = useState(0);
+//   const [setsCount, setSetsCount] = useState(0);
 
 
-export default function Workouts(props) {
+//   const exercises = [
+//     {
+//       id: 1,
+//       name: "arms",
+//       sets: 3,
+//       reps: 15,
+//     },
+//     {
+//       id: 2,
+//       name: "legs",
+//       sets: 2,
+//       reps: 15,
+//     },
+//     {
+//       id: 3,
+//       name: "shoulders",
+//       sets: 5,
+//       reps: 15,
+//     },
+//   ];
+//   //add exercises to workout
+//   const addToWorkouts = (item) => {
+//     setaddExerciseList([...exerciseList, item]);
+//   };
 
-  const [exerciseList, setaddExerciseList] = useState([]);
-  console.log("this is the added Items", exerciseList);
-  const [repCount, setRepCount] = useState(0);
-  const [setsCount, setSetsCount] = useState(0);
+//   //remove Items from the Workout list
+//   const removeFromWorkouts = (item) => {
+//     let exerciselistCopy = [...exerciseList];
+//     exerciselistCopy = exerciselistCopy.filter(workoutItems => workoutItems.id != item.id);
+//     setaddExerciseList(exerciselistCopy);
+//   };
 
+//   const exerciseItems = exercises.map((item) => (
+//     <div key={item.id}>
+//       {`${item.name} Sets:${item.sets}, Reps:${item.reps}`}
+//       <input type='submit' value='add' onClick={() => addToWorkouts(item)} />
+//     </div>
 
-  const exercises = [
-    {
-      id: 1,
-      name: "arms",
-      sets: 3,
-      reps: 15,
-    },
-    {
-      id: 2,
-      name: "legs",
-      sets: 2,
-      reps: 15,
-    },
-    {
-      id: 3,
-      name: "shoulders",
-      sets: 5,
-      reps: 15,
-    },
-  ];
-  //add exercises to workout
-  const addToWorkouts = (item) => {
-    setaddExerciseList([...exerciseList, item]);
-  };
+//   ));
 
-  //remove Items from the Workout list
-  const removeFromWorkouts = (item) => {
-    let exerciselistCopy = [...exerciseList];
-    exerciselistCopy = exerciselistCopy.filter(workoutItems => workoutItems.id != item.id);
-    setaddExerciseList(exerciselistCopy);
-  };
+//   const workoutItems = exerciseList.map((item) => (
+//     <div key={item.id}> {`${item.name} Sets:${item.sets}, Reps:${item.reps}`}
+//       <input type='submit' value='add' onClick={() => removeFromWorkouts(item)} />
+//     </div>
 
-  const exerciseItems = exercises.map((item) => (
-    <div key={item.id}>
-      {`${item.name} Sets:${item.sets}, Reps:${item.reps}`}
-      <input type='submit' value='add' onClick={() => addToWorkouts(item)} />
-    </div>
+//   ));
 
-  ));
+//   return (
+//     <div>
+//       Exercises
+//       <div>{exerciseItems}</div>
 
-  const workoutItems = exerciseList.map((item) => (
-    <div key={item.id}> {`${item.name} Sets:${item.sets}, Reps:${item.reps}`}
-      <input type='submit' value='add' onClick={() => removeFromWorkouts(item)} />
-    </div>
-
-  ));
-
-  return (
-    <div>
-      Exercises
-      <div className="text-black">{exerciseItems}</div>
-
-    Workout
-    <div className="text-black">{workoutItems}</div>
+//     Workout
+//     <div>{workoutItems}</div>
     
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 

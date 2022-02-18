623751db8917493c0c24319b7b713cefcfa0c2f4
RiazK <RiazCKhan@gmail.com>
2022-02-18 14:49:24 -0500

    Increase number of items in mock data

--- react-front-end/src/components/ExerciseList.js
+++ react-front-end/src/components/ExerciseList.js
@@ -105,6 +105,100 @@ const backExercises = [
     id: "12",
     name: "assisted pull-up",
     target: "lats"
+  },
+  {
+    bodyPart: "back",
+    equipment: "cable",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0007.gif",
+    id: "13",
+    name: "alternate lateral pulldown",
+    target: "lats"
+  },
+  {
+    bodyPart: "back",
+    equipment: "body weight",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3293.gif",
+    id: "14",
+    name: "archer pull up",
+    target: "lats"
+  },
+  {
+    bodyPart: "back",
+    equipment: "leverage machine",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0015.gif",
+    id: "15",
+    name: "assisted parallel close grip pull-up",
+    target: "lats"
+  },
+  {
+    bodyPart: "back",
+    equipment: "leverage machine",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0017.gif",
+    id: "16",
+    name: "assisted pull-up",
+    target: "lats"
+  }, {
+    bodyPart: "back",
+    equipment: "cable",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0007.gif",
+    id: "17",
+    name: "alternate lateral pulldown",
+    target: "lats"
+  },
+  {
+    bodyPart: "back",
+    equipment: "body weight",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3293.gif",
+    id: "18",
+    name: "archer pull up",
+    target: "lats"
+  },
+  {
+    bodyPart: "back",
+    equipment: "leverage machine",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0015.gif",
+    id: "19",
+    name: "assisted parallel close grip pull-up",
+    target: "lats"
+  },
+  {
+    bodyPart: "back",
+    equipment: "leverage machine",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0017.gif",
+    id: "20",
+    name: "assisted pull-up",
+    target: "lats"
+  }, {
+    bodyPart: "back",
+    equipment: "cable",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0007.gif",
+    id: "21",
+    name: "alternate lateral pulldown",
+    target: "lats"
+  },
+  {
+    bodyPart: "back",
+    equipment: "body weight",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3293.gif",
+    id: "22",
+    name: "archer pull up",
+    target: "lats"
+  },
+  {
+    bodyPart: "back",
+    equipment: "leverage machine",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0015.gif",
+    id: "23",
+    name: "assisted parallel close grip pull-up",
+    target: "lats"
+  },
+  {
+    bodyPart: "back",
+    equipment: "leverage machine",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0017.gif",
+    id: "24",
+    name: "assisted pull-up",
+    target: "lats"
   }
 ];
 

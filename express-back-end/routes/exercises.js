e550a7427cb4bcd2acf85c1fe7ac4cd44c93282b
Lendl Peralta <janlendl@gmail.com>
2022-02-17 21:34:08 -0700

    Update query string and properly passed the variables needed for the query

--- express-back-end/routes/exercises.js
+++ express-back-end/routes/exercises.js
@@ -4,27 +4,32 @@ module.exports = db => {
   router.get("/exercises", (req, res) => {
     db.query(`SELECT * FROM exercises`)
       .then(({ rows: exercises }) => {
-      res.json(exercises);
-    });
+        res.json(exercises);
+      });
   });
 
   // update exercise sets and reps
-  router.put("/editExercise/", (req, res) => {
-    console.log("FE",req.body)
-    // const {values, id} = req.body.exercise
+  router.post("/editExercise/", (req, res) => {
+    console.log("Data passed from Front-End: ", req.body)
+    const updateExercise = req.body.exerciseData;
+    const sets = updateExercise.sets;
+    const reps = updateExercise.reps;
+    const id = updateExercise.id;
+
     db.query(`
-    UPDATE exercises
-    SET number_of_sets = $1::int, number_of_reps = $2::int
-    WHERE id = $3::int;
-    `[sets, reps, id]
+      UPDATE exercises
+      SET number_of_sets = $1, number_of_reps = $2
+      WHERE exercises.id = $3;
+      `, [sets, reps, id]
     )
-    .then(() => {
-      setTimeout(() => {
-        res.status(204).json({res});
-        console.log("Exercise Reps and Sets Updated!");
-      }, 1000);      
-    })
-    .catch(error => console.log('Error', error));
+      .then(() => {
+        setTimeout(() => {
+          res.status(204).json({});
+          console.log("Exercise Reps and Sets Updated!");
+        }, 1000);
+      })
+      .catch(error => console.log('Error', error.message));
+
   })
 
   return router;

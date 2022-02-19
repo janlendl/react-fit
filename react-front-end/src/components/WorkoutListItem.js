2b9309e4953c5cb1b0a0f364728b4758ceff61d4
Lendl Peralta <janlendl@gmail.com>
2022-02-18 21:12:33 -0700

    Add onDelete function, add delete button

--- react-front-end/src/components/WorkoutListItem.js
+++ react-front-end/src/components/WorkoutListItem.js
@@ -1,20 +1,37 @@
-import './WorkoutListItem.scss';
+import axios from 'axios';
 import ExerciseListItem from './ExerciseListItem';
+import './WorkoutListItem.scss';
 import "./Exercises.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 
 export default function WorkoutListItem(props) {
 
+  const onDelete = () => {
+    const id = props.id
+
+    axios.delete(`/api/deleteWorkout/${id}`)
+      .then(() => {
+        console.log("Sending data for deletion")
+      })
+      .catch((error) => {
+        console.log("Error: ", error)
+      });
+
+  };
+
   return (
     <>
       <div className="container background margin-2">
+        <button className="btn btn-primary m-1 float-right" onClick={() => onDelete(props.id)}><FontAwesomeIcon icon={faTrash} /></button>
         <div className="card-header text-center">
           <h2 className="capitalize">{props.workoutName}</h2>
-          <sm>Date Created: {props.dateCreated}</sm>
+          <p>Date Created: {props.dateCreated}</p>
         </div>
 
         <div className="card-text align-center px-4">
-          {props.exercises.map((exercise,i) => {
+          {props.exercises.map((exercise, i) => {
 
             return (
               <ExerciseListItem

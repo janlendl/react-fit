7ab839db64bfb6601f4cc2e71b0809bee894301e
Lendl Peralta <janlendl@gmail.com>
2022-02-20 12:28:55 -0700

    Add a new state to track the state change for the sets and reps on the input

--- react-front-end/src/components/EditWorkout.js
+++ react-front-end/src/components/EditWorkout.js
@@ -3,23 +3,26 @@ import { useState } from "react";
 import "./Exercises.scss";
 
 export default function EditWorkout(props) {
-
-  const [sets, setSets] = useState(props.sets);
-  const [reps, setReps] = useState(props.reps);
   
+  const { sets, reps, setSets, setReps } = props;
+  const [inputSets, setInputSets] = useState(sets);
+  const [inputReps, setInputReps] = useState(reps);
+
+
   const onSubmit = (event) => {
     event.preventDefault();
-    
+
     const values = {
-      sets,
-      reps,
+      sets:inputSets,
+      reps:inputReps,
     }
     const exerciseData = { ...values, id: JSON.stringify(props.id) };
 
-    axios.post('/api/editExercise/', { exerciseData })
+    axios.put('/api/editExercise/', { exerciseData })
       .then((res) => {
-
         console.log("Sending data to backend: ", exerciseData);
+        setSets(inputSets);
+        setReps(inputReps);
 
       }).catch((error) => {
         console.log(error);
@@ -34,19 +37,19 @@ export default function EditWorkout(props) {
         <form onSubmit={onSubmit} >
 
           <label>Sets</label>
- 
+
           <input
             type="text"
-            defaultValue={sets}
-            onChange={(event) => setSets(event.target.value)}
+            value={inputSets}
+            onChange={(event) => setInputSets(event.target.value)}
             name="sets"
             className=" d-inline-flex col-3 mx-1" />
 
           <label>Reps</label>
           <input
             type="text"
-            defaultValue={reps}
-            onChange={(event) => setReps(event.target.value)}
+            value={inputReps}
+            onChange={(event) => setInputReps(event.target.value)}
             name="reps"
             className="d-inline col-3 mx-1 mb-3" />
           <button type="submit" className="btn whitetxt">Submit</button>

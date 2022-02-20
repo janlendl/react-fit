7755b31465840fec56b940e7585932a310aea137
gabmor38 <Gabriela.morenor@gmail.co>
2022-02-19 21:32:05 -0500

    css changed for SignUp

--- react-front-end/src/components/SignUp.js
+++ react-front-end/src/components/SignUp.js
@@ -1,5 +1,5 @@
 
-import "./CategoriesStyle.scss"
+import "./SignUp.scss"
 
 export default function SignUp() {
 
@@ -7,50 +7,50 @@ export default function SignUp() {
     <>
     <section className="containerMain categoriesWrapper">
 
-      <article className="landingPageCategories">
+      <div className="landingPageCategories">
           <img src="/back.jpeg" />
           <div className="title topleftSM">Back</div> 
-      </article>
+      </div>
 
-      <article className="landingPageCategories">  
+      <div className="landingPageCategories">  
           <img src="/cardio.jpeg" />
           <div className="title topleftSM">Cardio</div>
-      </article>
+      </div>
 
-      <article className="landingPageCategories">
+      <div className="landingPageCategories">
           <img src="/chest.jpeg" />
           <div className="title topleftSM">Chest</div>
-      </article>
+      </div>
 
-      <article className="landingPageCategories">
+      <div className="landingPageCategories">
           <img src="/lower_arm.jpeg" />
           <div className="title topleft">Lower Arms</div>
-      </article>
+      </div>
 
-      <article className="landingPageCategories">
+      <div className="landingPageCategories">
           <img src="/lower_leg.jpeg" />
           <div className="title topleft">Lower Legs</div>
-      </article>
+      </div>
 
-      <article className="landingPageCategories">
+      <div className="landingPageCategories">
           <img src="/shoulder.jpeg" />
           <div className="title topleft">Shoulders</div>
-      </article>
+      </div>
 
-      <article className="landingPageCategories">
+      <div className="landingPageCategories">
           <img src="/upper_arm.jpeg" />
           <div className="title topleft">Upper Arms</div>
-      </article>
+      </div>
 
-      <article className="landingPageCategories">
+      <div className="landingPageCategories">
           <img src="/upper_leg.jpeg" />
           <div className="title topleft">Upper Legs</div>
-      </article>
+      </div>
 
-      <article className="landingPageCategories">
+      <div className="landingPageCategories">
           <img src="/core.jpeg" />
           <div className="title topleftSM">Core</div>
-      </article>
+      </div>
 
       <div className="d-flex"><button className="btn btn-lg">GET STARTED</button></div>
     </section>

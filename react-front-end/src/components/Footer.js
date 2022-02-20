424633fbab1dc8e0483f8fd6cad085bef8d9f40e
gabmor38 <Gabriela.morenor@gmail.co>
2022-02-19 21:42:22 -0500

    added footer wording

--- react-front-end/src/components/Footer.js
+++ react-front-end/src/components/Footer.js
@@ -10,7 +10,7 @@ export default function Footer(children) {
   return (
     <div>
       <div className="footer">
-      <p className="m-2" style={{color:'black'}}>Made with <FontAwesomeIcon icon={faHeart} style={{color:'black'}}/></p>
+      <p className="m-2" style={{color:'black'}}>Made with <FontAwesomeIcon icon={faHeart} style={{color:'black'}}/> by LRGG</p>
       </div>
       <Outlet />
     </div>

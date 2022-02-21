97709c1142c2380dee3197b20bdcb6a02f337b56
gabmor38 <Gabriela.morenor@gmail.co>
2022-02-20 21:01:19 -0500

    Created signup page

--- react-front-end/src/components/Navbar.js
+++ react-front-end/src/components/Navbar.js
@@ -7,6 +7,7 @@ import Footer from "./Footer";
 export default function Navbar(children) {
 
 
+  
   return (
     <>
       <header>
--- react-front-end/src/components/SignUp.js
+++ react-front-end/src/components/SignUp.js
@@ -5,62 +5,62 @@ export default function SignUp() {
 
   return (
     <>
-    <div className="container m-4">
-      <div>
-      <img src="/back.jpeg" alt="" />
-
-      </div>
-      </div>
-      <section className="containerMain ">
-        <div className="containerMain categoriesWrapper">
-          <div className="landingPageCategories">
+      <section>
+       
+        <div className="SignUp_shadow">
+        <img src="/cover2.jpeg" alt="" className="topImg"/>
+        </div>
+       
+        <div className="categoriesWrapper padding-50 purpleBckgnd">
+          <div className="signCategories">
             <img src="/back.jpeg" alt="" />
             <div className="title topleftSM">Back</div>
           </div>
 
-          <div className="landingPageCategories">
+          <div className="signCategories">
             <img src="/cardio.jpeg" alt=""/>
             <div className="title topleftSM">Cardio</div>
           </div>
 
-          <div className="landingPageCategories">
+          <div className="signCategories">
             <img src="/chest.jpeg" alt=""/>
             <div className="title topleftSM">Chest</div>
           </div>
 
-          <div className="landingPageCategories">
+          <div className="signCategories">
             <img src="/lower_arm.jpeg" alt=""/>
             <div className="title topleft">Lower Arms</div>
           </div>
 
-          <div className="landingPageCategories">
+          <div className="signCategories">
             <img src="/lower_leg.jpeg" alt=""/>
             <div className="title topleft">Lower Legs</div>
           </div>
 
-          <div className="landingPageCategories">
+          <div className="signCategories">
             <img src="/shoulder.jpeg" alt=""/>
             <div className="title topleft">Shoulders</div>
           </div>
 
-          <div className="landingPageCategories">
+          <div className="signCategories">
             <img src="/upper_arm.jpeg" alt=""/>
             <div className="title topleft">Upper Arms</div>
           </div>
 
-          <div className="landingPageCategories">
+          <div className="signCategories">
             <img src="/upper_leg.jpeg" alt=""/>
             <div className="title topleft">Upper Legs</div>
           </div>
 
-          <div className="landingPageCategories">
+          <div className="signCategories">
             <img src="/core.jpeg" alt=""/>
             <div className="title topleftSM">Core</div>
           </div>
-          <div className="landingPageCategories col-12 ">
+          <div className="signCategories col-12 ">
             <button className="btn btn-lg border-0 text_color p-4 fw-bold">GET STARTED</button>
-          </div>`
+          </div>
         </div>
+      
 
       </section>
      
